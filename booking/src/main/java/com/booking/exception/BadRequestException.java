f7c7a290ab1c43a37da6b833023cfced050c1d98
Ashok921 <90255130+Ashok921@users.noreply.github.com>
2022-03-21 13:29:16 +0530

    added few classes

--- booking/src/main/java/com/booking/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.booking.exception;
-
-public class BadRequestException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public BadRequestException(String errorMessage) {
-		super(errorMessage);
-	}
-}
